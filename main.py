4cc38c11f7842922a0f7363b2b09cf118dbab457
HEMANTH <hemanthannamneedi05@gmail.com>
2026-09-23 14:26:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-# coding & Extended task:
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 app = FastAPI()
+students = {
+    1: {
+        "name": "Hemanth",
+        "age": 22,
+        "course": "Python"
+    },
+    2: {
+        "name": "Rahul",
+        "age": 21,
+        "course": "Data Science"
+    },
+    3: {
+        "name": "Suresh",
+        "age": 23,
+        "course": "Web Development"
+    }
+}
 @app.get("/")
 def home():
-    return{
-        "message": "Welcome to FastAPI application"
-    }
-
-@app.get("/profile")
-def profile():
-    return{
-        "name": "Hemanth",
-        "internship_title": "AI/ML Intern"
-    }
-
-@app.get("/hello/{name}")
-def hello(name:str):
     return {
-        "message": f"HEllo, {name}!"
+        "message": "Student API"
     }
+@app.get("/students")
+def get_students():
+    return students
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+    if student_id not in students:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+    return students[student_id]
